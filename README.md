71aa3aa1b9f70b8c9a84b76557dfde5e030fdce5
-akku- <151517253+akku1139@users.noreply.github.com>
2024-12-14 18:37:24 +0900

    wip

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # web-reader
 A simple and lightweight viewer for viewing websites in text only
+
+## Note
+
+### HTMLRewriter
+
+https://zenn.dev/maretol/articles/ad576aff797513
+
+https://developers.cloudflare.com/workers/runtime-apis/html-rewriter/
+
+## Readability
+
+https://github.com/mozilla/readability
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import { Hono } from "hono"
+import { validator } from "hono/validator"
+import { Readability } from "@mozilla/readability"
 
 const app = new Hono()
+.get("/read",
+  validator("query", (v, c) => {
+    const url = v["url"]
+    if (!url) {
+      return // error
+    }
+    if (Array.isArray(url)) {
+      return
+    }
+    if (!URL.canParse(url)) {
+      return {
+        url
+      }
+    }
+  }),
+  async c => {
+    const { url } = c.req.valid("query")
+  }
+)
+
+export default app
