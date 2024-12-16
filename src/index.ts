3db7c71b1cc1f67d99de0d3eeb65bf316547b885
-akku- <151517253+akku1139@users.noreply.github.com>
2024-12-16 16:11:19 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,22 @@ const app = new Hono()
   validator("query", (v, c) => {
     const url = v["url"]
     if (!url) {
-      return // error
+      return c.text("error", 400)
     }
     if (Array.isArray(url)) {
-      return
+      return c.text("error", 400)
     }
     if (!URL.canParse(url)) {
       return {
         url
       }
     }
+    return c.text("error", 400)
   }),
   async c => {
     const { url } = c.req.valid("query")
+    fetch(url)
+    new HTMLRewriter()
   }
 )
 
