953e9c3030a41a2005e6d95138108e6074d7a62a
-akku- <151517253+akku1139@users.noreply.github.com>
2024-12-31 00:09:51 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ const app = new Hono()
 .get("/read",
   validator("query", (v, c) => {
     const url = v["url"]
-    console.log("debug url:", url)
     if (!url) {
       return c.text("error (URL is not set)", 400)
     }
@@ -15,11 +14,11 @@ const app = new Hono()
       return c.text("error (Multiple URLs are not allowed)", 400)
     }
     if (!URL.canParse(url)) {
-      return {
-        url
-      }
+      return c.text("error (Cannot parse URL)", 400)
     }
-    return c.text("error (Cannot parse URL)", 400)
+    return {
+      url
+    }
   }),
   async c => {
     const { url } = c.req.valid("query")
